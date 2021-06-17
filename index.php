c8c1fae11a04e3421a92e207d8ff5143692617ee
229942 Ludwisiak Patryk <229942@edu.p.lodz.pl>
2021-06-17 17:01:58 +0200

    updating repo content

--- index.php
+++ index.php
@@ -1,209 +1,24 @@
 <?php
-session_start();
+	session_start();
+        if($_SESSION["userID"]==null){
+            header("Location: ./login.php");
+        }
 ?>
-<!DOCTYPE html>
-<html lang="pl-PL">
+
+<!DOCTYPE HTML>
+<html lang="pl">
 <head>
+	<meta charset="utf-8">
 
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
+	<title>Przychodnia</title>
 
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-    <title> BOIVET - Strona główna </title>
-    <meta charset="utf-8"/>
+	<link rel="stylesheet" href="style.css">
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="mystyle.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style>
-
-        .navbar {
-            margin-bottom: 0;
-            border-radius: 0;
-        }
-
-        footer {
-            background-color: #f2f2f2;
-            padding: 25px;
-        }
-
-        .carousel-inner img {
-            width: 100%;
-            margin: auto;
-            min-height: 200px;
-        }
-
-
-    </style>
 </head>
 <body>
 
-<script>
-
-    $(document).ready(function () {
-        $('li.active').removeClass('active');
-        $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
-    });
-
-</script>
-
-<!-- menu -->
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a class="current" href="index.php">STRONA GŁÓWNA</a></li>
-                <li><a href="about.php">O NAS</a></li>
-                <li><a href="services.php">ZAKRES ŚWIADCZEŃ</a></li>
-                <li><a href="contact.php">KONTAKT I DOJAZD</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <?php
-                if (isset($_SESSION['username'])) {
-
-                    if ($_SESSION['status'] == "client") {
-                        echo '<li><a href="visit.php"><span class="glyphicon glyphicon-log-in"></span> UMÓW WIZYTĘ</a></li>';
-                    }
-                    if ($_SESSION['status'] == "doctor") {
-                        echo '<li><a href="article.php"><span class="glyphicon glyphicon-log-in"></span> DODAJ ARTYKUŁ</a></li>';
-                    }
-
-                } else {
-                    echo '<li><a href="register.php"><span class="glyphicon glyphicon-log-in"></span> REJESTRACJA</a></li>';
-                }
-                ?>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-
-<div class="container text-center gap">
-    <div class="row well">
-        <div class="col-sm-8">
-            <p class="big titel"><b>W skrócie</b></p>
-            <p class="left">Prowadzimy lecznicę weterynaryjną dla zwierząt na łódzkim śródmieściu, gdzie nasi
-                weterynarze w gabinetach prowadzą klinikę weterynaryjną.
-                Prowadzimy również wizyty domowe dla zwierząt. Istniejemy od 2018 roku. Minione lata sprawiły, że
-                udało
-                nam się pomóc wielu zwierzętom ku radości ich właścicieli.
-                W trakcie codziennej pracy nie zawsze jest łatwo, ale tym większe towarzyszą nam chwile wzruszenia,
-                a i
-                nie ukrywamy, że i dumy, gdy udaje się wygrać z czasem i chorobą lub odsunąć widmo śmierci.
-                Nasi pacjenci zawsze mogą liczyć na wszechstronną, rzetelną opiekę.
-                Oferujemy szeroki wachlarz usług medycznych dzięki wyszkolonej kadrze weterynarzy, mocno osadzonych
-                na
-                filarach: wiedzy, chęci, potrzeby serca i zaangażowania.
-                Dokładamy wszelkich starań, by spieszyć z pomocą (w lecznicy i podczas domowych wizyt), wspierając
-                się
-                bogatym doświadczeniem, podnoszeniem własnych kwalifikacji i nieustającą praktyką.
-                Żywimy głębokie przekonanie, że zdołamy sprostać wszelkim wyzwaniom, w tym i najtrudniejszemu, czyli
-                Państwa oczekiwaniom! A zatem...<br>
-                Zapraszamy!</p>
-            <img width="85%" height="85%" src="img/doggo.jpg">
-        </div>
-        <div class="col-sm-4">
-
-            <?php
-
-            if (!isset($_SESSION['username'])) {
-
-
-                echo '<div class="well" id="panel">';
-                if (isset($_SESSION['signup_success'])) {
-
-                    echo 'Rejestracja przebiegła pomyślnie. Możesz się zalogować';
-                    unset($_SESSION['signup_success']);
-                }
-                echo <<<END
-                <p class="logowanie">LOGOWANIE
-                    <form action="signin.php" method="post">
-                        <label>Nazwa użytkownika:</label>
-                        <input type="text" id="username" name="login">
-                        <label>Hasło:</label>
-                        <input type="password" id="password" name="password">
-END;
-                if (isset($_SESSION['error'])) {
-
-                    echo $_SESSION['error'];
-                    session_unset();
-
-                }
-
-                
-                echo '<div id="lower">';
-                echo '<input type="submit" value="ZALOGUJ">';
-                echo '</div>';
-                echo '</form>';
-                echo '</div>';
-                echo '</p>';
-            } else {
-
-                echo '<div class="well" id="panel">';
-
-                echo '<br>';
-                echo '<p class="logowanie">Witaj ' . $_SESSION['username'] . '!</p>';
-
-                if ($_SESSION['status'] == "client") {
-
-                    echo <<<END
-             <a href="visits.php"><input id="username" type="button" value="Umówione wizyty"></a>
-             <br><br>
-             <a href="visit.php"><input id="username" type="button" value="Umów wizytę"></a>
-             <br><br>
-             <a href = "animals.php"><input id="username" type="button" value="Twoje zwierzaki"></a>
-             <br><br>
-             <a href="animal.php"><input id="username" type="button" value="Dodaj zwierzaka"></a>
-             <a href="logout.php">
-                <div id="lower">
-                    <input type="submit" value="Wyloguj się">
-                </div>
-             </a>
-END;
-                }
-                if ($_SESSION['status'] == "doctor") {
-
-                    echo <<<END
-             <a href="visits.php"><input id="username" type="button" value="Umówione wizyty"></a>
-             <br><br>
-             <a href="articles.php"><input id="username" type="button" value="Twoje artykuły"></a>
-             <br><br>
-             <a href="article.php"><input id="username" type="button" value="Dodaj artykuł"></a>
-             <a href="logout.php">
-                <div id="lower">
-                    <input type="submit" value="Wyloguj się">
-                </div>
-             </a>
-END;
-
-                }
-                echo '</div>';
-
-            }
-            ?>
-
-        </div>
-    </div>
-</div>
-</div>
-</div><br>
-
-<footer class="container-fluid text-center">
-    <p>Inżynieria Oprogramowania</p>
-</footer>
+	<input type="button" value="Zwierzęta"  class="redirBtn" onClick="document.location.href='./zwierzeta.php'" />
+	<input type="button" value="Użytkownik" class="redirBtn" onClick="document.location.href='./user.php'" />
 
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,445 +1,56 @@
 <?php
-
-session_start();
-
-if(isset($_SESSION['username'])){
-    header('Location: index.php');
-}
-
-if (isset($_POST['login'])) {
-
-    $accept = true;
-
-    //LOGIN CHECK
-    $login = $_POST['login'];
-    if ((strlen($login) < 3) || (strlen($login) > 99)) {
-
-        $accept = false;
-        $_SESSION['err_login'] = "Login musi posiadać pomiędzy 3 a 99 znaków!";
-
-    } else {
-        unset($_SESSION['err_login']);
-    }
-
-    if (!ctype_alnum($login)) {
-
-        $accept = false;
-        $_SESSION['err_login'] = "Login musi się składać tylko z liter i cyfr(bez polskich znaków)!";
-
-    } else {
-        unset($_SESSION['err_login']);
-    }
-
-    //EMAIL CHECK
-    $email = $_POST['email'];
-    $emailS = filter_var($email, FILTER_SANITIZE_EMAIL);
-    if ((filter_var($emailS, FILTER_VALIDATE_EMAIL)) == false || ($email != $emailS)) {
-        $accept = false;
-        $_SESSION['err_email'] = "Padaj poprawny e-mail";
-    } else {
-        unset($_SESSION['err_email']);
-    }
-
-    //PASSWORD CHECK
-    $password = $_POST['password'];
-    $repassword = $_POST['rePassword'];
-    if (strlen($password) < 7 || strlen($password) > 50) {
-        $accept = false;
-        $_SESSION['err_password'] = "Hasło powinno zawierać od 6 do 50 znaków";
-    } else {
-        unset($_SESSION['err_password']);
-    }
-
-    if ($password != $repassword) {
-        $accept = false;
-        $_SESSION['err_repassword'] = "Hasła nie są identyczne";
-    } else {
-        unset($_SESSION['err_repassword']);
-    }
-
-    //USERNAME CHECK
-    $username = $_POST['username'];
-    if (!ctype_alnum($username)) {
-        $accept = false;
-        $_SESSION['err_username'] = "Login musi się składać tylko z liter i cyfr(bez polskich znaków)";
-    } else {
-        unset($_SESSION['err_username']);
-    }
-
-    //NAME CHECK
-    $name = $_POST['name'];
-    if (strlen($name) < 1) {
-        $accept = false;
-        $_SESSION['err_name'] = "Wypełnij to pole";
-    } else {
-        unset($_SESSION['err_name']);
-    }
-
-    //SURNAME CHECK
-    $surname = $_POST['surname'];
-    if (strlen($surname) < 1) {
-        $accept = false;
-        $_SESSION['err_surname'] = "Wypełnij to pole";
-    } else {
-        unset($_SESSION['err_surname']);
-    }
-
-    //POSTAL CHECK
-    $postal = $_POST['postal'];
-    if (strlen($postal) < 1) {
-        $accept = false;
-        $_SESSION['err_postal'] = "Wypełnij to pole";
-    } else {
-        unset($_SESSION['err_postal']);
-    }
-
-    //CITY CHECK
-    $city = $_POST['city'];
-    if (strlen($city) < 1) {
-        $accept = false;
-        $_SESSION['err_city'] = "Wypełnij to pole";
-    } else {
-        unset($_SESSION['err_city']);
-    }
-
-    //ADRESS CHECK
-    $adress = $_POST['adress'];
-    if (strlen($adress) < 1) {
-        $accept = false;
-        $_SESSION['err_adress'] = "Wypełnij to pole";
-    } else {
-        unset($_SESSION['err_adress']);
-    }
-
-    //TELEPHONE NR CHECK
-    $tel = $_POST['tel'];
-    if (strlen($tel) < 1) {
-        $accept = false;
-        $_SESSION['err_tel'] = "Wypełnij to pole";
-    } else {
-        unset($_SESSION['err_tel']);
-    }
-
-    //TERMS ACCEPT CHECK
-    if (!isset($_POST['terms'])) {
-        $accept = false;
-        $_SESSION['err_terms'] = "Zaakceptuj regulamin";
-    } else {
-        unset($_SESSION['err_terms']);
-    }
-
-    //DATABASE CONNECTION
-    require_once "connect.php";
-    mysqli_report(MYSQLI_REPORT_STRICT);
-    try {
-        $connection = new mysqli($host, $db_user, $db_password, $db_name);
-        if ($connection->connect_errno != 0) {
-            throw new Exception(mysqli_connect_errno());
-        } else {
-            $result = $connection->query("SELECT id FROM users where email='$email'");
-
-            if (!$result) throw new Exception($connection->error);
-
-            $found = $result->num_rows;
-
-            if ($found > 0) {
-                $accept = false;
-                $_SESSION['err_email'] = "Istnieje już konto o takim adresie";
-            }
-
-            $result = $connection->query("SELECT id FROM users where login='$login'");
-
-            if (!$result) throw new Exception($connection->error);
-
-            $found = $result->num_rows;
-
-            if ($found > 0) {
-                $accept = false;
-                $_SESSION['err_login'] = "Istnieje już konto o takim loginie";
-            }
-
-            if ($accept) {
-
-                do {
-                    $id = rand(9999999, 99999999);
-                    $result = $connection->query("SELECT id FROM users where id='$id'");
-                } while ($result->num_rows > 1);
-
-                $password = password_hash($password, PASSWORD_DEFAULT);
-
-                if ($connection->query("INSERT INTO users VALUES('$id','$name','$surname','$postal','$adress','$city','$username','$email','$login','$password','client','$tel')")) {
-                    $_SESSION['signup_success'] = true;
-                    header('Location: index.php');
-                } else {
-                    throw new Exception($connection->error);
-                }
-            }
-            $connection->close();
-        }
-    } catch (Exeption $err) {
-        echo '<span style="color:darkred">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
-        exit();
-    }
-}
-
+    include 'Registering.php';
 ?>
-    <!DOCTYPE html>
-    <html lang="pl-PL">
-    <head>
-
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
-
-        <title> BOIVET - Rejestracja </title>
-        <meta charset="utf-8"/>
-
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="mystyle.css" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <style>
-
-            .navbar {
-                margin-bottom: 0;
-                border-radius: 0;
+<!DOCTYPE html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+	<?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $login = $_POST["login"];
+            $passwd = $_POST["passwd"];
+            $name = $_POST["name"];
+            $surname = $_POST["surname"];
+            $email = $_POST["email"];
+            $phone = $_POST["phone"];
+            $address = $_POST["address"];
+            $result = Registering::register($login, $passwd, $name, $surname, $email, $phone, $address);
+            switch ($result){
+                case 0:
+                    header("Location: index1.php");
+                    break;
+                case 1:
+                    print "Taki użytkownik już istnieje.";
             }
-
-            footer {
-                background-color: #f2f2f2;
-                padding: 25px;
-            }
-
-            .carousel-inner img {
-                width: 100%;
-                margin: auto;
-                min-height: 200px;
-            }
-
-
-        </style>
-    </head>
-    <body>
-
-    <script>
-
-        $(document).ready(function () {
-            $('li.active').removeClass('active');
-            $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
-        });
-
-    </script>
-
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">STRONA GŁÓWNA</a></li>
-                    <li><a href="about.php">O NAS</a></li>
-                    <li><a href="services.php">ZAKRES ŚWIADCZEŃ</a></li>
-                    <li><a href="contact.php">KONTAKT I DOJAZD</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <div class="container text-center gap2">
-        <div class="row">
-
-            <div class="col-sm-12">
-                <div class="well" id="panel">
-                    <p class="logowanie">REJESTRACJA
-                    <form method="post">
-
-
-                        <label>Login:</label>
-                        <br>
-                        <input type="text" id="password" name="login"
-                               value="<?php if (isset($_POST['email'])) echo $_POST['login'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_login'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_login'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>E-mail:</label>
-                        <br>
-                        <input type="text" id="password" name="email"
-                               value="<?php if (isset($_POST['email'])) echo $_POST['email'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_email'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_email'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Hasło:</label>
-                        <br>
-                        <input type="password" id="password" name="password">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_password'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_password'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Powtórz hasło:</label>
-                        <br>
-                        <input type="password" id="password" name="rePassword">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_repassword'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_repassword'] . '</p>';
-                        }
-
-                        ?>
-                        <br><br>
-
-                        <label>Dane osobowe:</label>
-
-                        <br><br><br>
-                        <label>Nazwa użytkownika:</label>
-                        <br>
-                        <input type="text" id="password" name="username"
-                               value="<?php if (isset($_POST['username'])) echo $_POST['username'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_username'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_username'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Imię:</label>
-                        <br>
-                        <input type="text" id="password" name="name"
-                               value="<?php if (isset($_POST['name'])) echo $_POST['name'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_name'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_name'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Nazwisko:</label>
-                        <br>
-                        <input type="text" id="password" name="surname"
-                               value="<?php if (isset($_POST['surname'])) echo $_POST['surname'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_surname'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_surname'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Kod pocztowy:</label>
-                        <br>
-                        <input type="text" id="password" name="postal"
-                               value="<?php if (isset($_POST['postal'])) echo $_POST['postal'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_postal'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_postal'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Adres zamieszkania:</label>
-                        <br>
-                        <input type="text" id="password" name="adress"
-                               value="<?php if (isset($_POST['adress'])) echo $_POST['adress'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_adress'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_adress'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Miejscowość:</label>
-                        <br>
-                        <input type="text" id="password" name="city"
-                               value="<?php if (isset($_POST['city'])) echo $_POST['city'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_city'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_city'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <label>Nr telefonu:</label>
-                        <br>
-                        <input type="text" id="password" name="tel"
-                               value="<?php if (isset($_POST['tel'])) echo $_POST['tel'] ?>">
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_tel'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_tel'] . '</p>';
-                        }
-
-                        ?>
-
-                        <br>
-                        <label><input type="checkbox" name="terms"> Akceptuję regulamin</label>
-
-                        <br>
-                        <?php
-
-                        if (isset($_SESSION['err_terms'])) {
-                            echo '<p style="color: darkred">' . $_SESSION['err_terms'] . '</p>';
-                        }
-
-                        ?>
-                        <br>
-                        <div id="lower">
-                            <input type="submit" value="ZAREJESTRUJ">
-                        </div>
-                    </form>
-                </div>
-                </p>
-            </div>
-        </div>
-    </div>
-    </div><br>
-
-    <footer class="container-fluid text-center">
-        <p>Inżynieria Oprogramowania</p>
-    </footer>
-
-    </body>
-    </html>
-<?php
-
-if (!@$accept) {
-    session_unset();
-    unset($_POST);
-}
-
-?>
+          }
+        ?>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+			<div>
+				<label for="login">Login</label><input id="login" name="login" type="text" placeholder="Login" required>
+			</div>
+			<div>
+				<label for="passwd">Hasło</label><input id="passwd" name="passwd" type="password" placeholder="Hasło" required>
+			</div>
+                        <div>
+				<label for="name">Imię</label><input id="name" name="name" type="text" placeholder="Jan" required>
+			</div>
+			<div>
+				<label for="surname">Nazwisko</label><input id="surname" name="surname" type="text" placeholder="Kowalski" required>
+			</div>
+                        <div>
+				<label for="email">Email</label><input id="email" name="email" type="text" placeholder="nazwa@adres.com" required>
+			</div>
+			<div>
+				<label for="phone">Telefon</label><input id="phone" name="phone" type="text" placeholder="+48123456789" required>
+			</div>
+			<div>
+				<label for="address">Adres</label><input id="address" name="address" type="text" placeholder="Prosta 1" required>
+			</div>
+                
+			<div>
+				<input id="submit" type="submit" value="Zarejestruj">
+			</div>
+		</form>
+	</body>
+</html>
